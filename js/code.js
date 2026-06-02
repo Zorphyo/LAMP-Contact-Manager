0f1d0a83fc4880bd1775c64c74b56169821f88b3
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-06-02 17:35:03 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -198,3 +198,52 @@ function searchColor()
 	}
 	
 }
+
+function searchContacts()
+{
+	let srch = document.getElementById("searchText").value;
+	document.getElementById("contactSearchResult").innerHTML = "";
+	
+	let contactList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/SearchContacts.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+				    let rowClass = (i % 2 == 0) ? "contactColor1" : "contactColor2";
+				
+				    contactList +=
+				        "<div class='contactRow " + rowClass + "'>" +
+				        "<strong>" + jsonObject.results[i].FirstName + " " +
+				        jsonObject.results[i].LastName + "</strong><br>" +
+				        jsonObject.results[i].Phone +
+						jsonObject.results[i].Email +
+				        "</div>";
+				}
+				
+				document.getElementById("contactList").innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+	
+}
